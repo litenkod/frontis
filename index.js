9fe7a985daeda0e510b1bde0e3db3d7ac3c7561b
litenkod <kod@litenkod.se>
2018-11-21 16:29:08 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 module.exports = function console2(element) {
-    if (string !== null) throw new TypeError("Frontis wants a something to log!");
+    console.log(element)
+    if (!element) throw new TypeError("Frontis wants a something to log!");
     const render = element.toString() + ': ' + element;
     return console.log(render);
 };
