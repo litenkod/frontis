c23c92ecc0c81e9f199d5c8a1cb71b7450086449
litenkod <kod@litenkod.se>
2018-11-21 15:06:32 +0100

    udate package.json and added index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+
+module.exports = function console2(element) {
+    if (string !== null) throw new TypeError("Frontis wants a something to log!");
+    const render = element.toString() + ': ' + element;
+    return console.log(render);
+};
