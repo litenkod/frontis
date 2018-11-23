25c1d861594da5967d3ad61966352c6a0ceda647
litenkod <kod@litenkod.se>
2018-11-23 16:18:41 +0100

    added more typeof

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 
-module.exports = {
-    console2: function(element) {
-        console.log(element)
-        if (!element) throw new TypeError("console2 wants something to log!");
-        const render = element.toString() + ': ' + element;
+function console2(element) {
+    if (!element) throw new TypeError("console2 wants something to log!");
 
-        return console.log(render);
+    let varType = element,
+        value = '';
+
+    if (typeof(element) == 'undefined') {
+        varType = 'undefined';
+        value = element;
+
+    } else if (Array.isArray(element)) {
+        varType = '[array]';
+        for (let i = 0; i < element.length; i++) {
+            value += i + ': ' + element[i] + '\n';
+        }
+
+    } else if (typeof(element) == 'object'){
+        varType = '{object}';
+        Object.entries(element).forEach(([key, val]) => {
+            value += 'key: ' + key + ', val: ' + val + '\n';
+        });
+
+    } else if(typeof(element) == 'string') {
+        varType = 'string';
+        value = element;
+
+    } else if(typeof(boolean)){
+        varType = 'boolean';
+        value = element;
+
+    } else{
+        varType = 'unknown';
+        value = element;
     }
+
+    return console.log(varType + '\n' + value);
 }
