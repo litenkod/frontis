4ce6658cf3273b8e203c0ffa744d6879c7af96d9
litenkod <kod@litenkod.se>
2018-11-25 00:38:09 +0100

    added missing export

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
 
-function console2(element) {
-    if (!element) throw new TypeError("console2 wants something to log!");
+module.exports = {
+    console2: function(element) {
+        if (!element) throw new TypeError("console2 wants something to log!");
 
-    let varType = element,
-        value = '';
+        let varType = element,
+            value = '';
 
-    if (typeof(element) == 'undefined') {
-        varType = 'undefined';
-        value = element;
+        if (typeof(element) == 'undefined') {
+            varType = 'undefined';
+            value = element;
 
-    } else if (Array.isArray(element)) {
-        varType = '[array]';
-        for (let i = 0; i < element.length; i++) {
-            value += i + ': ' + element[i] + '\n';
+        } else if (Array.isArray(element)) {
+            varType = '[array]';
+            for (let i = 0; i < element.length; i++) {
+                value += i + ': ' + element[i] + '\n';
+            }
+
+        } else if (typeof(element) == 'object'){
+            varType = '{object}';
+            Object.entries(element).forEach(([key, val]) => {
+                value += 'key: ' + key + ', val: ' + val + '\n';
+            });
+
+        } else if(typeof(element) == 'string') {
+            varType = 'string';
+            value = element;
+
+        } else if(typeof(boolean)){
+            varType = 'boolean';
+            value = element;
+
+        } else{
+            varType = 'unknown';
+            value = element;
         }
 
-    } else if (typeof(element) == 'object'){
-        varType = '{object}';
-        Object.entries(element).forEach(([key, val]) => {
-            value += 'key: ' + key + ', val: ' + val + '\n';
-        });
-
-    } else if(typeof(element) == 'string') {
-        varType = 'string';
-        value = element;
-
-    } else if(typeof(boolean)){
-        varType = 'boolean';
-        value = element;
-
-    } else{
-        varType = 'unknown';
-        value = element;
+        return console.log(varType + '\n' + value);
     }
-
-    return console.log(varType + '\n' + value);
 }
